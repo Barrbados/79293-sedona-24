c57e9f9c4b5882ad8db224c5723292487f89ca20
Barrbados <46927742+Barrbados@users.noreply.github.com>
2019-03-09 22:00:36 +0300

    добавил JS

---
+++ js/openform.js
@@ -0,0 +1,26 @@
+
+  var link = document.querySelector(".button-open-form");
+  var popup = document.querySelector(".index-search-form");
+  var checkin = document.querySelector("[name=check-in]");
+  var checkout = document.querySelector("[name=check-out]");
+  var adults = document.querySelector("[name=value-adults]");
+  var children = document.querySelector("[name=value-children]");
+  // var isStorageSupport = true;
+  // var storage = "no";
+  //
+  // try{
+  //
+  // }
+
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("search-form-open");
+  });
+  popup.addEventListener("submit", function (evt) {
+    if (!checkin.value || !checkout.value || !adults.value || !children.value){
+      evt.preventDefault();
+      popup.classList.remove("index-form-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("index-form-error");
+    }
+  });
